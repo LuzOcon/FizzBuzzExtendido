b05d0be1d30e866200736380ac3633f361a3b8fb
Luz Ocon <luzhdez711@gmail.com>
2025-09-02 21:17:25 -0600

    Tasks done excepto task 6

--- task6.js
+++ task6.js
@@ -1,5 +1,36 @@
 // Refer to Task 6 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
-  };
+const prompt = require("prompt-sync")();
+const numMax = prompt("Ingresa el numero de lineas a generar: ");
+
+for (let i = 1; i <= numMax; i++) {
+  //let array =[]
+  //let arrayNumGen = []
+  //arrayNumGen += array.push(i);
+      if (i%3===0 && i%5===0 && i%7===0){
+        console.log("FizzBuzzWoof");
+        }
+      else if (i%3===0 && i%5===0){
+        console.log("FizzBuzz");
+        }
+      else if (i%3===0 && i%7===0){
+        console.log("FizzWoof");
+        }
+      else if (i%5===0 && i%7===0){
+        console.log("BuzzWoof");
+        }
+      else if(i%3===0){
+        console.log("Fizz");
+      }
+      else if(i%5===0){
+        console.log("Buzz");
+      }
+      else if(i%7===0){
+        console.log("Woof");
+      }
+      else{
+        console.log(i);
+      }
+     //console.log(arrayNumGen);
+}  
+ 
--- task7.js
+++ task7.js
@@ -1,14 +1,43 @@
 // Refer to Task 7 in your Instructions to complete this task
 
 let buzzWords = [
-    "Fizz",
-    "Buzz",
-    "Woof",
-    "Bark",
-    "Awoo",
-    "Bang"
+    "Rosa",
+    "Tulipan",
+    "Girasol",
   ];
   
-  for (let i = 0; i < 1; i++) {
-    console.log("This is Task Seven!");
-  };
+const prompt = require("prompt-sync")();
+const numMax = +prompt("Ingresa el numero de lineas a generar: ");
+
+for (let i = 1; i <= numMax; i++) {
+    let output = "";
+
+    if (i%3===0 && i%5===0 && i%7===0) {
+        output = "FizzBuzzWoof";
+    }
+    else if (i%3===0 && i%5===0) {
+        output = "FizzBuzz";
+    }
+    else if (i%3===0 && i%7===0) {
+        output = "FizzWoof";
+    }
+    else if (i%5===0 && i%7===0) {
+        output = "BuzzWoof";
+    }
+    else if (i%3===0) {
+        output = "Fizz";
+    }
+    else if (i%5===0) {
+        output = "Buzz";
+    }
+    else if (i%7===0) {
+        output = "Woof";
+    }
+    else {
+        output = i.toString();
+    }
+
+    output = output.replaceAll("Fizz", buzzWords[0]).replaceAll("Buzz", buzzWords[1]).replaceAll("Woof", buzzWords[2]);
+
+    console.log(output);
+}
